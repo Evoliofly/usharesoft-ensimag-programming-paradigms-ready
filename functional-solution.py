fc66f2b1144490c2de94d176eaab37fc275246a4
Antoine Murat <antoine.murat@etu.unilim.fr>
2018-11-22 18:26:42 +0100

    Update functional-solution.py

--- functional-solution.py
+++ functional-solution.py
@@ -2,4 +2,4 @@
 import json
 j=open('beer_list.json')
 B=json.load(j)
-print(max((b for b in B if b['Tx_Alcool']), key=lambda b: b['Tx_Alcool'])['Nom'])
+print(max((b for b in B if b['Tx_Alcool']),key=lambda b:b['Tx_Alcool'])['Nom'])
