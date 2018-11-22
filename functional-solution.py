dd64cc9337a3cf621b29176d020ef8bbf9e71bbf
Antoine Murat <antoine.murat@etu.unilim.fr>
2018-11-23 00:03:59 +0100

    124 octets

--- functional-solution.py
+++ functional-solution.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 import json
 s='Tx_Alcool'
-print(max(json.load(open('beer_list.json')),key=lambda b:[b[s],0][not b[s]])['Nom'])
+print(max(json.load(open('beer_list.json')),key=lambda b:b[s] or 0)['Nom'])
